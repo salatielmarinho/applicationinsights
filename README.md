1bf0a054c3bcf805792a61693c95c1f496805f84
salatielmarinho <salatielmarinho@gmail.com>
2024-12-11 18:28:34 -0300

    subida inicial

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# applicationinsights
+# ASPNETCore8-MVC-AzureEventHubs-AppInsightsConnString_Enquete
+Exemplo de geração de eventos envolvendo o voto em uma enquete sobre o dia favorito da semana, utilizando o Azure Event Hubs em uma Web App criada com o .NET 8 + ASP.NET Core. Inclui o uso de um Dockerfile para geração de imagens Linux, além de monitoramento com o Azure Application Insights (utilizando Connection String).
+
+## Author
+
+:boy: [Salatiel Marinho](https://github.com/salatielmarinho)
---
+++ src/SiteQuestaoEventHub/Controllers/VotacaoController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SiteQuestaoEventHub.EventHubs;
+
+namespace SiteQuestaoEventHub.Controllers;
+
+public class VotacaoController : Controller
+{
+    private readonly ILogger<VotacaoController> _logger;
+    private readonly VotacaoProducer _producer;
+
+    public VotacaoController(ILogger<VotacaoController> logger,
+        VotacaoProducer producer)
+    {
+        _logger = logger;
+        _producer = producer;
+    }
+
+    public async Task<IActionResult> VotoSegundaFeira()
+    {
+        return await ProcessarVoto("SegundaFeira");
+    }
+
+    public async Task<IActionResult> VotoSextaFeira()
+    {
+        return await ProcessarVoto("SextaFeira");
+    }
+
+    public async Task<IActionResult> VotoSabado()
+    {
+        return await ProcessarVoto("Sabado");
+    }
+
+    public async Task<IActionResult> VotoDomingo()
+    {
+        return await ProcessarVoto("Domingo");
+    }
+
+    private async Task<IActionResult> ProcessarVoto(string interesse)
+    {
+        _logger.LogInformation($"Processando voto para o interesse: {interesse}");
+        await _producer.Send(interesse);
+        _logger.LogInformation($"Informações sobre o voto '{interesse}' enviadas para o Azure Event Hubs!");
+
+        TempData["Voto"] = interesse;
+        return RedirectToAction("Index", "Home", new { voto = interesse });
+    }
+}
